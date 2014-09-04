c9c703c39f96c92d3b3f8b50277b09871f4ae2cc
adamdotg <fNkXican@adamgarciasmbp2.home>
2014-09-03 20:35:20 -0500

    init

---
+++ js/app_2.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$('.start').click(function(){
+		alert("What is this");
+	});
+		/*for (var i = 0; i < 100; i++){
+			if (i % 15 === 0){
+				$("body").append("<p>" + "fizz buzz");
+			}	else if (i % 5 === 0){				
+				$("body").append("<p>" + "buzz");
+			}	else if (i % 3 === 0){
+				$("body").append("<p>" + "fizz");
+			}	else {
+				$("body").append("<p>" + i "</p>");
+			}
+			
+		}*/
+
+		
+});
+
+
