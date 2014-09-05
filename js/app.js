5b90c11f95cb06ebf3652b9ed440ddc045b5e53d
adamdotg <adamdotg@gmail.com>
2014-09-05 08:04:00 -0500

    Mentor session mods

--- js/app.js
+++ js/app.js
@@ -1,32 +1,57 @@
 $(document).ready(function(){
-	$('button').click(function() {
-		var collect = $('#fbNum').val();
-		/*var collect = prompt('Please enter a number.');*/
+var message = "";
+var collect = 17;
+function verify (collect){
+	var bool = true;
 		if(isNaN(collect)){
-			alert('This is not a number');
+			message = 'This is not a number';
+			bool = false;
 		}	else if (collect % 1 !=0) {
-			alert('No decimals allowed');
+			message = 'No decimals allowed';
+			bool = false;
+		}	else if (collect <= 0){
+			message = 'Enter positive numbers';
+			bool = false;
+		}	else if (collect > 1000){
+			message = 'Too big';
+			bool = false;	
 		}	else {
-			var x = +collect;
-			alert(x + ' is your number');
+			message = collect + ' is your number';
 			console.log(collect);
-			// start named function
-			/*var fizzBuzz = function(){
-	
-			};
-			*/
-			for (var i = 1; i <= x; i++) {
-    			if(i % 15 === 0) {
-       	 			$('body').append('<p>fizz buzz</p>');
-      			} else if (i % 3 === 0) {
-        			$('body').append('<p>fizz</p>');
-      			} else if (i % 5 === 0) {
-        			$('body').append('<p>buzz</p>');
-      			} else {
-        			$('body').append('<p>' + i + '</p>');
-      			}
+		}	
+		return bool;
+	}
+	var fizzBuzz = function(collect){
+		var x = collect;
+		for (var i = 1; i <= x; i++) {
+			if(i % 3 === 0 && i % 5 === 0) {
+		 			$('.results').append('<p>fizz buzz</p>');
+			} else if (i % 3 === 0) {
+				$('.results').append('<p>fizz</p>');
+			} else if (i % 5 === 0) {
+				$('.results').append('<p>buzz</p>');
+			} else {
+				$('.results').append('<p>' + i + '</p>');
 			}
-			// end named function
 		}
+	};
+	var handler = function (){
+		var collect = $('#fbNum').val();
+		/*$('.results p').remove();*/
+		$('.results').html(" ");
+		if ($.trim(collect) != "") {
+			collect = +collect;
+			if (verify(collect)) {
+				fizzBuzz(collect);
+			};
+		};
+		/*console.log(message);*/
+		$('.message-holder').text(message);
+	};
+	$('button').click(handler);
+	$('#fbNum').keypress(function(event){
+		if(event.which == 13){
+			handler();
+		};
 	});
-});
+});	
